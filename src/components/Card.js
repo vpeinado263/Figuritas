2a180b33f9a9c4920c2d1501b4feab0cd5d86f74
Victor Peinado <vpeinado263@gmail.com>
2024-10-24 15:59:01 -0300

    Finalizar componente Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,17 +4,17 @@ const Card = () => {
     return(
         <>
         <figure>
-            <img src="https://http2.mlstatic.com/D_NQ_NP_745969-MLA70234211612_072023-O.webp" />
+            <img src="https://dcdn.mitiendanube.com/stores/001/539/668/products/colchon-antiescaras21-60e5d4495d1e7736b216132282351257-1024-1024.jpg" />
             <figcaption>
-                <h3>Colchon Aire Antiescaras Tubular Reforzado Obeso 200 Kilos</h3>
+                <h3>Colchon Aire Antiescaras</h3>
             </figcaption>
             <p>Colchón de material PVC atóxico, 18 Celdas individuales tubulares, 2 extensiones no inflables para doblarse debajo del colchón, cabecera y piecera. Apto para camas ortopédicas y camas tradicionales de 1 plaza</p>
             <Button/>
         </figure>
         <style jsx>{`
         figure {
-            width: 200px;
-            height: 275px;
+            width: 300px;
+            height: 450px;
             border: 2px solid black;
             display: flex;
             flex-direction: column;
@@ -22,7 +22,7 @@ const Card = () => {
         }
         img {
             width: 100%;
-            height: 45%;
+            height: 50%;
             object-fit: cover;
         }
         h3 {
